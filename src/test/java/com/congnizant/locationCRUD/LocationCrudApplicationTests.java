a5950cbe93dcc8f229faaca15be1cdad40405c9f
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-06-30 15:02:23 -0400

    build,gradle update with maven

--- src/test/java/com/congnizant/locationCRUD/LocationCrudApplicationTests.java
+++ src/test/java/com/congnizant/locationCRUD/LocationCrudApplicationTests.java
@@ -2,16 +2,11 @@ package com.congnizant.locationCRUD;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class LocationCrudApplicationTests {
 
 	@Test
-	void contextLoads(ApplicationContext context) {
-		assertThat(context).isNotNull();
+	void contextLoads() {
 	}
-
 }

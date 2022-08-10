30108bc4a676ce7095aedb9ceba0a5e47d53b37b
abelAsres <abelasres35@gmail.com>
2022-08-10 17:38:37 -0400

    refactor getDistances

--- src/main/java/com/congnizant/locationCRUD/controller/LocationController.java
+++ src/main/java/com/congnizant/locationCRUD/controller/LocationController.java
@@ -1,10 +1,8 @@
 package com.congnizant.locationCRUD.controller;
 
-import com.congnizant.locationCRUD.models.GeonameData;
 import com.congnizant.locationCRUD.models.Location;
 import com.congnizant.locationCRUD.models.ResponseGeoNameData;
 import com.congnizant.locationCRUD.service.LocationService;
-import org.hibernate.annotations.Cache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
@@ -12,56 +10,51 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.persistence.EntityNotFoundException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @CrossOrigin(originPatterns = "*", exposedHeaders = "*", allowedHeaders = "*")
 public class LocationController {
-    @Autowired
-    LocationService locationService;
+  @Autowired
+  LocationService locationService;
 
-    @GetMapping("/locations")
-    @Cacheable(value = "locations")
-    public ResponseEntity<?> getLocation(@RequestParam(required=false) String name) {
-        try {
-            if (name == null || name.equals("")) {
-                return ResponseEntity.ok(locationService.getAllLocations());
+  @GetMapping("/locations")
+  @Cacheable(value = "locations")
+  public ResponseEntity<?> getLocation(@RequestParam(required = false) String name) {
+    try {
+      if (name == null || name.equals("")) {
+        return ResponseEntity.ok(locationService.getAllLocations());
 
-            }else {
-                Location location = locationService.findByName(name);
-                return ResponseEntity.ok(location);
-            }
+      } else {
+        Location location = locationService.findByName(name);
+        return ResponseEntity.ok(location);
+      }
 
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(404).body(e.getMessage());
-        }
+    } catch (EntityNotFoundException e) {
+      return ResponseEntity.status(404).body(e.getMessage());
     }
+  }
 
-    @GetMapping("/locations/{id}")
-    @Cacheable(value = "locations", key = "#id")
-    public ResponseEntity<?> getLocation(@PathVariable("id") int id) {
-        try {
-            Location location = locationService.findById(id);
-            return ResponseEntity.ok(location);
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(404).body(e.getMessage());
-        }
+  @GetMapping("/locations/{id}")
+  @Cacheable(value = "locations", key = "#id")
+  public ResponseEntity<?> getLocation(@PathVariable("id") int id) {
+    try {
+      Location location = locationService.findById(id);
+      return ResponseEntity.ok(location);
+    } catch (EntityNotFoundException e) {
+      return ResponseEntity.status(404).body(e.getMessage());
     }
-    @GetMapping("/geoname")
-    public ResponseEntity<?> getZipcodes(@RequestParam String zipcode,@RequestParam String country ,@RequestParam int radius){
-        String url="http://api.geonames.org/findNearbyPostalCodesJSON?postalcode="+
-                zipcode+"&country="+
-                country+"&radius="+
-                radius+"&username=deepakAgarwal";
-        RestTemplate restTemplate=new RestTemplate();
-          ResponseGeoNameData geonameData = restTemplate.getForObject(url,ResponseGeoNameData.class);
-          locationService.addDistances(geonameData);
-          return ResponseEntity.ok(geonameData);
+  }
 
-
-    }
+  @GetMapping("/geoname")
+  public ResponseEntity<?> getZipcodes(@RequestParam String zipcode, @RequestParam String country, @RequestParam int radius) {
+    String url = "http://api.geonames.org/findNearbyPostalCodesJSON?postalcode=" +
+        zipcode + "&country=" +
+        country + "&radius=" +
+        radius + "&username=deepakAgarwal";
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseGeoNameData geonameData = restTemplate.getForObject(url, ResponseGeoNameData.class);
+    return ResponseEntity.ok(locationService.addDistances(geonameData));
+  }
 
 //    @GetMapping("/locations/zipcodes")
 //    public ResponseEntity<?> getAllZipcodes() {
--- src/main/java/com/congnizant/locationCRUD/service/LocationService.java
+++ src/main/java/com/congnizant/locationCRUD/service/LocationService.java
@@ -1,12 +1,10 @@
 package com.congnizant.locationCRUD.service;
 
 import com.congnizant.locationCRUD.dao.LocationRepository;
-import com.congnizant.locationCRUD.models.GeonameData;
 import com.congnizant.locationCRUD.models.Location;
 import com.congnizant.locationCRUD.models.ResponseGeoNameData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
@@ -14,32 +12,32 @@ import java.util.Optional;
 
 @Service
 public class LocationService {
-    @Autowired
-    LocationRepository locationRepository;
+  @Autowired
+  LocationRepository locationRepository;
 
-    public Location findByName(String name) {
-        Location location = locationRepository.findByName(name);
-        if (location == null) {
-            throw new EntityNotFoundException(name + " does not exist");
-        }
-        System.out.println("call the database");
-        return location;
+  public Location findByName(String name) {
+    Location location = locationRepository.findByName(name);
+    if (location == null) {
+      throw new EntityNotFoundException(name + " does not exist");
     }
+    System.out.println("call the database");
+    return location;
+  }
 
-    public Location findById(int id) {
-        Optional<Location> location = locationRepository.findById(id);
-        if (!location.isPresent()) {
-            throw new EntityNotFoundException("location with id " + id + " does not exist");
-        }
-        System.out.println("call the database");
-        return location.get();
+  public Location findById(int id) {
+    Optional<Location> location = locationRepository.findById(id);
+    if (!location.isPresent()) {
+      throw new EntityNotFoundException("location with id " + id + " does not exist");
     }
+    System.out.println("call the database");
+    return location.get();
+  }
 
-    public List<Location> getAllLocations(){
-        return locationRepository.findAll();
-    }
+  public List<Location> getAllLocations() {
+    return locationRepository.findAll();
+  }
 
-//    public List<String> getAllZipcodes() {
+  //    public List<String> getAllZipcodes() {
 //        List<String> allZipcode = locationRepository.findAllZipcodes();
 //        if (allZipcode.isEmpty()) {
 //            throw new EntityNotFoundException("Error");
@@ -47,21 +45,17 @@ public class LocationService {
 //        System.out.println(allZipcode);
 //        return allZipcode;
 //    }
-    public List<Location> addDistances(ResponseGeoNameData responseGeoNameData){
-       List<Location> locations= this.getAllLocations();
-//       locations.stream().filter(location -> responseGeoNameData.getPostalCodes().contains(location.getZipcode()))
-        responseGeoNameData.getPostalCodes().stream().filter(geonameData -> locations.stream().anyMatch(location -> location.getZipcode().equals(geonameData.getPostalCode()))).forEach(geonameData ->
-                        locations.stream().flatMap(location -> {
-                            if(location.getZipcode().equals(geonameData.getPostalCode())){
-                                location.setDistance(geonameData.getDistance());
-                            }
-                            return  null;
-                        })
-                );
-        System.out.println(locations);
-        return  null;
+  public List<Location> addDistances(ResponseGeoNameData responseGeoNameData) {
+    List<Location> locations = this.getAllLocations();
 
-
-    }
+    locations.stream().forEach(location ->
+        responseGeoNameData.getPostalCodes().stream().filter(geoNameData ->
+            location.getZipcode().equals(geoNameData.getPostalCode())
+        ).forEach(geoNameData ->
+            location.setDistance(geoNameData.getDistance())
+        )
+    );
+    return locations;
+  }
 
 }
